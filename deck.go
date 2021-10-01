0c14be61cab16dbc4066c4636ad579a17df3b539
U2PIA <u2pia.2019@gmail.com>
2021-10-01 16:55:08 +0900

    base code

---
+++ deck.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+// deck이라는 새로운 변수를 만듬. 걔는 []string 타입임.
+type deck []string
+
+// receiver function
+//  d ==> 변수명 --> 관례적으로 receiver 인자는 단어 하나(또는 둘)를 씀.
+//  deck ==> 변수타입
+//  print ==> function name
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+// 일반 함수 선언과 함 비교해 보렴
+// func fuckCard() string {
+// 	return "다이아몬드 5"
+// }
+
+// 2중 for loop를 이용해 deck을 만드는 함수
+func newDeck() deck {
+	cards := deck{} //deck type으로 empty 된 변수 선언
+	cardSuits := []string{"스페이드", "다이아몬드", "하트", "클로버"}
+	cardValues := []string{"A", "2", "3", "4"}
+
+	// for loop에서 index를 안 쓸거면 '_'를 넣음.
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+//////////////////////////////////
+// Step 7: 함수에서..multiple return을 알아보자.
+// 	- 여러개 리턴할 수 있다.
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+//////////////////////////////////
+// Step 8: 파일저장
+//	- receiver로 만들자.
+//	- slice를 합쳐보자.
+
+func (d deck) toString() string {
+	// deck이 어차피 []string 이라 전혀 문제 없음.
+	return strings.Join([]string(d), ",")
+
+}
+
+// return 타입을 'error'로 넣을 수 있음. 왜냐! WriteFile의 return type이 error니까.. 대단한 이유 없다.
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+//////////////////////////////////
+// Step 9: 파일읽기
+
+func newDeckfromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1) // 프로그램 종료하려면
+		return nil // nil을 리턴해 주려면
+	} else {
+		// str1 := string(bs)
+		// str2 := strings.Split(str1, ",")
+		// fmt.Println(str2)
+		return deck(strings.Split(string(bs), ","))
+	}
+}
---
+++ main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"fmt"
+)
+
+/* 기본
+  - Go는 Object Oriented 언어가 아님.
+    - 근데 흉내낼 수 있음?
+
+/****************
+  GO의 기본 Type
+   - bool
+   - string
+   - int
+   - float64
+   - array (incl. slice)
+   - map
+
+  근디 type 키워드를 이용해 새로운 변수를 만들 수 있음.
+
+
+*****************/
+
+/*****************
+* 배열(Array)
+  - Fixed length
+* Slice
+  - 일종의 배열
+  - Grow/shrink 가능
+  - 안에 있는 아이템들은 모두 같은 타입을 가져야 함.
+******************/
+
+func main() {
+
+	//////////////////////////////////
+	// Step 1
+	//////////////////////////////////
+	fmt.Println("Hello Fucker")
+
+	//////////////////////////////////
+	// Step 2
+	//////////////////////////////////
+	card := "스페이드 A" // var card string = "스페이드 A"
+	// 위에 있는 거랑 같은 효과. 단, 컴파일러가 알아서 변수를 할당하는 것에 차이가 있음.
+	// 단, 처음에 선언할 떄만 :을 넣어주고, 이후에 사용할때는 일반 변수와 똑같이 사용함.
+
+	fmt.Println(card)
+
+	//////////////////////////////////
+	// Step 3
+	//////////////////////////////////
+	new_card := newCard()
+	fmt.Println(new_card)
+
+	//////////////////////////////////
+	// Step 4
+	//////////////////////////////////
+
+	/***********************
+	* slice
+	  - slice의 선언은 "[]type{}"을 이용
+	  - 초기값을 넣으려면 {} 에 늘어 놓음.
+	  - slice에 값을 넣으려면 append 함수를 이용
+	  - 일반 배열처럼 slice[0] 등으로 접근 가능
+	***********************/
+
+	cards := []string{newCard(), newCard()}
+
+	//
+	// 그런데, 조금 방식이 평소와는 다르군..
+	cards = append(cards, "fucker")
+
+	fmt.Println(cards)
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+
+	//////////////////////////////////
+	// Step 5
+	//////////////////////////////////
+	// 새로운 type을 선언하고, receiver를 이용해 보자.
+	deck_cards := deck{newCard(), newCard()}
+	for i, card := range deck_cards {
+		fmt.Println(i, card)
+	}
+	deck_cards.print()
+
+	//////////////////////////////////
+	// Step 6: slice를 쪼개는 것을 해 보자
+	// - card[0:2] ==> 0, 1 번째 것만 끊어옴. 앞이 0일 경우 생략가능
+	// - card[2:] ==> 2번째부터 끝까지
+	//////////////////////////////////
+	fmt.Println("Step 6 ============================")
+	deck := newDeck()
+	deck.print()
+
+	//////////////////////////////////
+	// Step 7: 함수에서 multiple return을 알아보자.
+	fmt.Println("Step 7 ============================")
+	hand, remainingDeck := deal(deck, 5)
+	hand.print()
+	remainingDeck.print()
+
+	//////////////////////////////////
+	// Step 8: 파일저장
+	// 	- Type conversion (string/.../... ==> []byte )
+	//	- string str을 conversion 하려면,
+	// 		-[]byte(str)
+	// deck --> []string --> string ---> []byte 의 단계를 거쳐야 함.
+	fmt.Println("Step 8 ============================")
+	fmt.Println(deck.toString())
+	deck.saveToFile("fucker.sav")
+
+	//////////////////////////////////
+	// Step 9: 파일읽기
+	fmt.Println("Step 9 ============================")
+	deck2 := newDeckfromFile("fucker.sav")
+	deck2.print()
+}
+
+// 함수 선언
+func newCard() string {
+	return "다이아몬드 5"
+}
